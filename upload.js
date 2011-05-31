f2623507969700b010bd16e70ae50bab13e4db70
max <max@ubuntu.localdomain>
2011-05-30 23:57:36 -0700

    prepared storing uploaded file to riak, added some todos

--- upload.js
+++ upload.js
@@ -2,6 +2,7 @@ require.paths.push(__dirname + '/node_modules');
 
 var express = require('express'),
     redis = require('redis'),
+    riak = require('riak-js'),
     form = require('connect-form');
 
 var app = express.createServer(form({ keepExtensions: true }));
@@ -13,6 +14,9 @@ app.use(express.cookieParser());
 app.use(express.session({ secret: 'oi098ahsd789jlkdasl', store: new RedisStore() }));
 
 var redisPubSubClient = redis.createClient();
+
+// @TODO would be nice to do some round-robin here/later on in order to not always connect to the same node
+var riakClient = riak.getClient({ host: '127.0.0.1', port: 8010 });
 
 // render page with upload form
 app.get('/', function(req, res) {
@@ -29,6 +33,7 @@ app.get('/upload-form', function(req, res) {
 
 // upload posted file
 app.post('/', function(req, res, next) {
+    // @ TOD check mimetype && size of uploaded image
     var uploadSessionId = req.session.uploadSessionId;
     var lastPercent = 0;
     
@@ -46,13 +51,24 @@ app.post('/', function(req, res, next) {
     });
 
     req.form.complete(function(err, fields, files) {
+        console.log('\nuploaded %s to %s',  files.image.filename, files.image.path);
+
         if (err) {
             redisPubSubClient.publish('upload:session:' + uploadSessionId, JSON.stringify({ type: 'upload-failed' }));
             next(err);
         } else {
-            redisPubSubClient.publish('upload:session:' + uploadSessionId, JSON.stringify({ type: 'upload-success' }));
-            console.log('\nuploaded %s to %s',  files.image.filename, files.image.path);
-            res.redirect('back');
+            fs.readFile(files.image.path, 'binary', function(err, image) {
+                if (err) {
+                    redisPubSubClient.publish('upload:session:' + uploadSessionId, JSON.stringify({ type: 'upload-failed' }));
+                    next(err);
+                } else {
+                    // @TODO maybe store some custom data
+                    // @TODO use correct mimetype
+                    riakClient.save('images', files.image.filename, image, { contentType: 'jpeg' });
+                    redisPubSubClient.publish('upload:session:' + uploadSessionId, JSON.stringify({ type: 'upload-success' }));
+                    res.redirect('back');
+                }
+            });
         }
     });
 });
